22a0edd9f5d4fdbde3b219cdb0a48ea273304b2f
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2019-12-26 12:57:53 +0100

    added 'game' generator to salnitro module

--- salnitro.erl
+++ salnitro.erl
@@ -6,3 +6,7 @@ player(Name, Class, Deck) ->
       deck => Deck, hand => [], discard => [], damage => 0, burnout => 1,
       type => player, class => Class, power_used => 0, power_per_turn => 0,
       power_cost => 2, attacks_per_turn => 1, attacks_this_turn => 0}.
+
+game(P1, P2) ->
+    #{p1 => P1, p2 => P2,
+      active => lists:nth(rand:uniform(2), [p1, p2])}.
